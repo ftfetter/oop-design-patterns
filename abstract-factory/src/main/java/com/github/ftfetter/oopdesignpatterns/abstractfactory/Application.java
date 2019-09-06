ca9a730c98c1038218952d4fe0433ec529fe3007
Felipe Fetter <felipetfetter@gmail.com>
2019-09-06 17:31:12 -0300

    Initial commit

---
+++ abstract-factory/src/main/java/com/github/ftfetter/oopdesignpatterns/abstractfactory/Application.java
@@ -0,0 +1,8 @@
+package com.github.ftfetter.oopdesignpatterns.abstractfactory;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("TODO");
+    }
+}
