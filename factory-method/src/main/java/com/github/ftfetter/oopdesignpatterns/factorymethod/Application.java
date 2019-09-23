4ae983396c7562b8ebcfbdf3e204ea1109da3510
Felipe Fetter <felipetfetter@gmail.com>
2019-09-23 09:51:14 -0300

    factory-method initial commit

---
+++ factory-method/src/main/java/com/github/ftfetter/oopdesignpatterns/factorymethod/Application.java
@@ -0,0 +1,8 @@
+package com.github.ftfetter.oopdesignpatterns.factorymethod;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("TODO");
+    }
+}
